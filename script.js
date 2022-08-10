876267bcc79b32be827ef09747d92b0331c35265
Carlos <carlosmsantos95@gmail.com>
2022-08-10 16:15:15 +0100

    completed basic operations

--- script.js
+++ script.js
@@ -2,46 +2,119 @@ const display = document.querySelector("#display-text");
 let number1 = "0";
 let number2 = "0";
 let switchToNumber2 = 0;
-
+let operation = "";
+let result = 0;
 
 function acOnClick(){
     display.innerHTML="0";
+    if(switchToNumber2===0){
+        number1 = "0";
+    }
+    else{
+        number2 = "0";
+    }
+    console.log("acOnClick number1:" + number1); 
+    console.log("acOnClick number2:" + number2);
+}
+function clearScreen(){
+    display.innerHTML="0";
     number1 = "0";
     number2 = "0";
+    switchToNumber2 = 0;
+    console.log("clearScreen number1:" + number1); 
+    console.log("clearScreen number2:" + number2);
 }
 
 function numberOnClick(value){
-    number1 +=value;
-    //eliminates the initial '0' in front of string
-    if(number1[0] === '0'){
-        number1 = number1.slice(1, number1.length);
+    if(switchToNumber2===0){
+        number1 +=value;
+        //eliminates the initial '0' in front of string
+        if(number1[0] === '0'){
+            number1 = number1.slice(1, number1.length);
+        }
+        //stops the string from overlaping the display
+        else if(number1.length>=11){
+            return;
+        }
+        display.innerHTML=number1;
     }
-    //stops the string from overlaping the display
-    else if(number1.length>=11){
-        return;
+    else{
+        number2 +=value;
+        if(number2[0] === '0'){
+            number2 = number2.slice(1, number2.length);
+        }
+        else if(number2.length>=11){
+            return;
+        }
+        display.innerHTML=number2;
     }
-    display.innerHTML=number1;
 }
 
+function addButton(){
+    console.log("number1:" + number1); 
+    console.log("number2:" + number2);
+    switchToNumber2 = 1;
+    operation="+";
+    display.innerHTML="0";
+}
+
+function subtractButton(){
+    console.log("number1:" + number1); 
+    console.log("number2:" + number2);
+    switchToNumber2 = 1;
+    operation="-";
+    display.innerHTML="0";
+}
+
+function multiplyButton(){
+    console.log("number1:" + number1); 
+    console.log("number2:" + number2);
+    switchToNumber2 = 1;
+    operation="*";
+    display.innerHTML="0";
+}
+
+function divideButton(){
+    console.log("number1:" + number1); 
+    console.log("number2:" + number2);
+    switchToNumber2 = 1;
+    operation="/";
+    display.innerHTML="0";
+}
+
+function showResult(){
+    operate(number1,number2,operation)
+}
+function printResult(result){
+    display.innerHTML= result.toString();
+    number1 = result;
+    number2 = "0";
+    console.log("result number1 "+number1);
+    console.log("result number2 "+number2);
+}
 function operate(num1,num2,operation){
     switch(operation) {
         case "+":
-            add(num1,num2);
-          break;
+            result = add(num1,num2);
+            printResult(result);
+            break;
         case "-":
-            subtract(num1,num2);
+            result = subtract(num1,num2);
+            printResult(result);
             break;
         case "*":
-            multiply(num1,num2);
+            result = multiply(num1,num2);
+            printResult(result);
             break;
         case "/":
-            divide(num1,num2);
+            result = divide(num1,num2);
+            printResult(result);
             break;
       } 
 }
 
 function add(num1,num2){
-    return num1+num2;
+    return parseInt(num1) + parseInt(num2);
 }
 
 function subtract(num1,num2){
